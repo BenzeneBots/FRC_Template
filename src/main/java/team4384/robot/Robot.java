91891a0c3acf7e60be811604912f1675796c5de3
Shivam-Panda <panda.shivam81@bloomfield.org>
2023-12-03 14:15:30 -0500

    FIRST

---
+++ src/main/java/team4384/robot/Robot.java
@@ -0,0 +1,34 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package team4384.robot;
+
+import com.ctre.phoenix.motorcontrol.TalonFXControlMode;
+import com.ctre.phoenix.motorcontrol.can.TalonFX;
+import edu.wpi.first.wpilibj.*;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+
+
+/**
+ * The VM is configured to automatically run this class, and to call the functions corresponding to
+ * each mode, as described in the TimedRobot documentation. If you change the name of this class or
+ * the package after creating this project, you must also update the build.gradle file in the
+ * project.
+ */
+public class Robot extends TimedRobot {
+
+    @Override
+    public void robotInit() {
+//        robo = new DifferentialDrive(left_1, right_1);
+    }
+
+    @Override
+    public void teleopPeriodic() {
+    }
+
+    public static void main(String... args) {
+        RobotBase.startRobot(Robot::new);
+    }
+}
